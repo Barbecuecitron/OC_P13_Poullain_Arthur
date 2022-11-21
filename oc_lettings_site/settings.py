04df4837b644159d717e04d88c42829bfa2521a6
BoiluTheGod <arthur.poullain@gmail.com>
2022-11-21 12:02:01 +0100

    prepared for db deployment

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -13,9 +13,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = "fp$9^593hsriajg$_%=5trot9g!1qa@ew(o-1#@=&4%=hp46(s"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False if os.environ.get("ENV") == "production" else True
 
-ALLOWED_HOSTS = ["*"]  # "localhost", "127.0.0.1", "*"]
+ALLOWED_HOSTS = [".heroku.com", "localhost", "127.0.0.1"]  # "localhost", "127.0.0.1", "*"]
 
 
 # Application definition
