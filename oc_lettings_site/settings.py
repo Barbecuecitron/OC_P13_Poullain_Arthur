5291de93bb36bff23951ad0edbd3a4372d315841
BoiluTheGod <arthur.poullain@gmail.com>
2022-11-21 11:52:25 +0100

    migrate db to pg from json fixture

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -69,8 +69,12 @@ WSGI_APPLICATION = "oc_lettings_site.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "oc-lettings-site.sqlite3"),
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "p13db",
+        "USER": "postgres",
+        "PASSWORD": "admin",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
