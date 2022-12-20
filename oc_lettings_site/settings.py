6ef3e58e4f50e2d33e7b14277f1048412205d182
BoiluTheGod <arthur.poullain@gmail.com>
2022-12-20 12:50:01 +0100

    sentry DSN added

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -2,8 +2,9 @@ import os
 import django_heroku
 import dj_database_url
 import environ
+import sentry_sdk
 from django.core.management.utils import get_random_secret_key
-
+from sentry_sdk.integrations.django import DjangoIntegration
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -136,3 +137,17 @@ STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 django_heroku.settings(locals())
+
+sentry_sdk.init(
+    dsn=env_vars("SENTRY_DSN"),
+    integrations=[
+        DjangoIntegration(),
+    ],
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0,
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True,
+)
