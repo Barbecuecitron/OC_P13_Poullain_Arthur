91eb6e63239ba9271e3a9dda188c13fbfe3a1b33
BoiluTheGod <arthur.poullain@gmail.com>
2023-02-15 16:35:02 +0100

    removed error sentry testing

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -2,15 +2,9 @@ from django.contrib import admin
 from django.urls import path, include
 
 
-def test():
-    division = 1 / 0
-    print(division)
-
-
 urlpatterns = [
     path("", include("home.urls")),
     path("lettings/", include("lettings.urls")),
     path("profiles/", include("profiles.urls")),
     path("admin/", admin.site.urls),
-    path("test", test),
 ]
