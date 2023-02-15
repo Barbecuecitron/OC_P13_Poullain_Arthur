44e4ea7621e6b5950c526517f290442e118d3528
BoiluTheGod <arthur.poullain@gmail.com>
2023-02-15 16:15:29 +0100

    error triggering for  sentry

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -2,9 +2,14 @@ from django.contrib import admin
 from django.urls import path, include
 
 
+def test():
+    division = 1 / 0
+
+
 urlpatterns = [
     path("", include("home.urls")),
     path("lettings/", include("lettings.urls")),
     path("profiles/", include("profiles.urls")),
     path("admin/", admin.site.urls),
+    path("test", test),
 ]
